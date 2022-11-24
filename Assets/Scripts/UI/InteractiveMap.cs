60ab4104615e05168ef341ab0649fd658396b323
Ray-Catty <rayzhou113@qq.com>
2022-11-24 22:04:42 +0800

    ui update

---
+++ Assets/Scripts/UI/InteractiveMap.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractiveMap : MonoBehaviour
+{
+
+    void Update()
+    {
+        
+    }
+}
