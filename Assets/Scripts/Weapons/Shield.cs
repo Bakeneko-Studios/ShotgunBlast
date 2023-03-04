33e6761340f45ca7b91089a6893a929e563921fa
LucasWang <100511265+Novigradian@users.noreply.github.com>
2023-03-04 18:46:34 +0800

    shield update
    shield is semitransparent and turns white when taking damage

--- Assets/Scripts/Weapons/Shield.cs
+++ Assets/Scripts/Weapons/Shield.cs
@@ -14,7 +14,15 @@ public class Shield : MonoBehaviour
     {
         rend = GetComponent<Renderer>();
         max_health = health;
-        rend.material.color = new Color(0f, 0f, 0.33f, 1f);
+        rend.material.color = new Color(0.4f, 0.6f, 1f, 0.6f);
+    }
+
+    IEnumerator takeDamage()
+    {
+        rend.material.color = new Color(1f, 1f, 1f, 1f);
+        Debug.Log("color changed");
+        yield return new WaitForSeconds(0.21f);
+        rend.material.color = new Color((0.4f + (max_health - health) / max_health) * 0.6f, (health / max_health) * 0.6f, 0.2f + (health / max_health) * 0.8f, 0.6f);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,7 +33,8 @@ public class Shield : MonoBehaviour
             dmg=other.gameObject.GetComponent<Pellet>().dmg;
             health -= dmg;
             //TODO: Add take damage animation?
-            rend.material.color = new Color(((max_health-health)/max_health)*0.85f, 0f, (health/(max_health*3)), 1f);
+            StartCoroutine(takeDamage());
+            //rend.material.color = new Color((0.4f+(max_health-health)/max_health)*0.6f, (health / max_health) * 0.6f, 0.2f+(health/max_health)*0.8f, 0.6f);
         }
     }
 
