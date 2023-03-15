a7eb5f8cee9c7fe01b5b1d7bdbd6f2b6bccd3a80
Ray-Catty <rayzhou113@qq.com>
2023-03-15 13:19:34 +0800

    Shotgun sound + npc

--- Assets/Scripts/DialogueBox.cs
+++ Assets/Scripts/DialogueBox.cs
@@ -9,27 +9,45 @@ public class DialogueBox : MonoBehaviour
     public string[] dialogue;
     [SerializeField] private TMP_Text text;
     [SerializeField] private GameObject bar;
-    private int OnChat;
+    public int OnChat;
 
     movement mv;
 
-    void OnInteract() {
+    public void OnInteract() {
         OnChat = 0;
         bar.SetActive(true);
-        NextLine();
+        //NextLine();
+        StartCoroutine(AutoLine());
         var eventSystem = EventSystem.current;
         eventSystem.SetSelectedGameObject(bar);
         mv = movement.instance;
         mv.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
     }
 
-    public void NextLine() {
-        text.text = dialogue[OnChat];
-        if (OnChat < dialogue.Length) {
-            OnChat ++;
-        } else {
-            bar.SetActive(false);
-            mv.enabled = true;
+    // public void NextLine() {
+    //     text.text = dialogue[OnChat];
+    //     if (OnChat < dialogue.Length) {
+    //         OnChat ++;
+    //     } else {
+    //         bar.SetActive(false);
+    //         mv.enabled = true;
+    //         Cursor.lockState = CursorLockMode.Locked;
+    //     }
+    // }
+
+    IEnumerator AutoLine() {
+        yield return Running();
+        bar.SetActive(false);
+             mv.enabled = true;
+             Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    IEnumerator Running() {
+        while (OnChat < dialogue.Length) {
+            text.text = dialogue[OnChat];
+            OnChat++;
+            yield return new WaitForSeconds(3);
         }
     }
 }
