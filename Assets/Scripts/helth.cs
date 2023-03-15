7e0ed24a08b4b8ab2938ec4685771f669a7a8614
AndaDong <81626564+AndaDong@users.noreply.github.com>
2023-03-15 11:50:17 +0800

    health pack

--- Assets/Scripts/helth.cs
+++ Assets/Scripts/helth.cs
@@ -7,6 +7,8 @@ public class helth : MonoBehaviour
     public void healthPack()
     {
         //heal for 30 and destroy pbject
+        playerHealth.instance.ChangeHealth(30);
         //can be slow regeneration or instant heal
+        Destroy(this.gameObject);
     }
 }
