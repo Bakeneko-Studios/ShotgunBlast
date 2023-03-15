96b62ce7d8516f9107ff3883cb7277bd249181c2
Ray-Catty <rayzhou113@qq.com>
2023-03-15 09:41:15 +0800

    unstupid

--- Assets/Scripts/RestingGround.cs
+++ Assets/Scripts/RestingGround.cs
@@ -8,7 +8,8 @@ public class RestingGround : MonoBehaviour
     public Transform SpawnHere;
 
     void Awake() {
-        GameObject prefabPlayer = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/savedPlayer.prefab");
+        //GameObject prefabPlayer = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/savedPlayer.prefab");
+        GameObject prefabPlayer = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Player.prefab");
         GameObject player = Instantiate(prefabPlayer);
         player.transform.position = SpawnHere.position;
     }
