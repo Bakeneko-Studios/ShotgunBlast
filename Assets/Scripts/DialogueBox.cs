b1c0c7a91f42b86e9f2921f510ffb9a82986320c
Ray-Catty <rayzhou113@qq.com>
2023-03-15 11:19:53 +0800

    forgot some stuff

--- Assets/Scripts/DialogueBox.cs
+++ Assets/Scripts/DialogueBox.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class DialogueBox : MonoBehaviour
@@ -10,10 +11,16 @@ public class DialogueBox : MonoBehaviour
     [SerializeField] private GameObject bar;
     private int OnChat;
 
+    movement mv;
+
     void OnInteract() {
         OnChat = 0;
         bar.SetActive(true);
         NextLine();
+        var eventSystem = EventSystem.current;
+        eventSystem.SetSelectedGameObject(bar);
+        mv = movement.instance;
+        mv.enabled = false;
     }
 
     public void NextLine() {
@@ -22,6 +29,7 @@ public class DialogueBox : MonoBehaviour
             OnChat ++;
         } else {
             bar.SetActive(false);
+            mv.enabled = true;
         }
     }
 }
